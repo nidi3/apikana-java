3f33fb3aee72081e7562504eff8b614f3eb1bd33
Travis-CI <swisspush@post.ch>
2018-03-22 14:25:03 +0100

    add credentials for releasing

--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
 git checkout ${TRAVIS_BRANCH}
-git config --global user.email "swisspush@post.ch"
-git config --global user.name "Travis-CI"
+git config user.email "swisspush@post.ch"
+git config user.name "Travis-CI"
 git config credential.helper "store --file=.git/credentials"
-echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+echo "https://${GH_TOKEN2}:@github.com" > .git/credentials
 
 mvn --settings settings.xml -B release:prepare -Pdo-release -DreleaseVersion=0.4.15 -DdevelopmentVersion=0.4.16-SNAPSHOT
 mvn --settings settings.xml release:perform -Pdo-release
