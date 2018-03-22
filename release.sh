7387047d582855cf335cae1a95373e4680d67f71
Stefan Niederhauser <stefan.niederhauser.3@post.ch>
2018-03-22 13:46:42 +0100

    add credentials for releasing

--- release.sh
+++ release.sh
@@ -4,6 +4,9 @@ git config --global user.email "swisspush@post.ch"
 git config --global user.name "Travis-CI"
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+cat .git/credentials
+echo aaa
+git config --global --list
 
 mvn --settings settings.xml -B release:prepare -Pdo-release -DreleaseVersion=0.4.15 -DdevelopmentVersion=0.4.16-SNAPSHOT
 mvn --settings settings.xml release:perform -Pdo-release
