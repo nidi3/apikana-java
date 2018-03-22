8eed10510221f31f05a0cf71951d296cc9cc368e
Stefan Niederhauser <stefan.niederhauser.3@post.ch>
2018-03-22 11:38:16 +0100

    add credentials for releasing

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 echo "releasing..."
-mvn -B release:prepare -Pdo-release -DreleaseVersion=0.4.15 -DdevelopmentVersion=0.4.16-SNAPSHOT -DdryRun=true
-mvn release:perform -Pdo-release
+mvn --settings settings.xml -B release:prepare -Pdo-release -DreleaseVersion=0.4.15 -DdevelopmentVersion=0.4.16-SNAPSHOT
+mvn --settings settings.xml release:perform -Pdo-release
 
