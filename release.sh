723dbf9ab055b54ec51266eb4e51d798c4a1c870
Travis-CI <swisspush@post.ch>
2018-03-27 11:34:24 +0200

    use global git config

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 git checkout ${TRAVIS_BRANCH}
-git config user.email "swisspush@post.ch"
-git config user.name "Travis-CI"
-git config credential.helper "store --file=.git/credentials"
+git config --global user.email "swisspush@post.ch"
+git config --global user.name "Travis-CI"
+git config --global credential.helper "store --file=.git/credentials"
 echo https://${GH_TOKEN}:@github.com > .git/credentials
 
 mvn --settings settings.xml -B release:prepare -Pdo-release -DreleaseVersion=0.4.15 -DdevelopmentVersion=0.4.16-SNAPSHOT
