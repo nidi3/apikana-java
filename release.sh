fe38d66ee2af43c700d2f86731a175b1e18d3083
Travis-CI <swisspush@post.ch>
2018-04-03 15:56:57 +0200

    releasing should not trigger new release...

--- release.sh
+++ release.sh
@@ -1,11 +1,12 @@
+echo "Releasing..."
 git checkout ${TRAVIS_BRANCH}
 git config --global user.email "swisspush@post.ch"
 git config --global user.name "Travis-CI"
 git config --global credential.helper "store --file=.git/credentials"
 echo https://${GH_TOKEN}:@github.com > .git/credentials
 
-echo "Preparing release..."
+echo "Preparing maven release..."
 mvn --settings settings.xml -B release:prepare -Pdo-release -DreleaseVersion=0.4.16 -DdevelopmentVersion=0.4.17-SNAPSHOT
 
-echo "Performing release..."
+echo "Performing maven release..."
 mvn --settings settings.xml release:perform -Pdo-release
