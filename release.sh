34e0a0fefa2f6af3bcd45b368aaa46ca1a093745
Travis-CI <swisspush@post.ch>
2018-04-03 16:31:04 +0200

    releasing should not trigger new release...

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ git config --global credential.helper "store --file=.git/credentials"
 echo https://${GH_TOKEN}:@github.com > .git/credentials
 
 echo "Preparing maven release..."
-mvn --settings settings.xml -B release:prepare -Pdo-release -DreleaseVersion=0.4.16 -DdevelopmentVersion=0.4.17-SNAPSHOT
+mvn --settings settings.xml -B release:prepare -Pdo-release -DreleaseVersion=0.4.17 -DdevelopmentVersion=0.4.18-SNAPSHOT
 
 echo "Performing maven release..."
 mvn --settings settings.xml release:perform -Pdo-release
