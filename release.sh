9153c153586bb109ef176b64ea13c4b4efd7b8a1
Travis-CI <swisspush@post.ch>
2018-04-03 15:48:42 +0200

    releasing should not trigger new release...

--- release.sh
+++ release.sh
@@ -4,5 +4,8 @@ git config --global user.name "Travis-CI"
 git config --global credential.helper "store --file=.git/credentials"
 echo https://${GH_TOKEN}:@github.com > .git/credentials
 
-mvn --settings settings.xml -B release:prepare -Pdo-release -DreleaseVersion=0.4.15 -DdevelopmentVersion=0.4.16-SNAPSHOT
+echo "Preparing release..."
+mvn --settings settings.xml -B release:prepare -Pdo-release -DreleaseVersion=0.4.16 -DdevelopmentVersion=0.4.17-SNAPSHOT
+
+echo "Performing release..."
 mvn --settings settings.xml release:perform -Pdo-release
