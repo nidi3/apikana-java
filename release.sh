de69e263d0d53442c8d0371d230aa960e091d527
Tom Winterhalder <tom.winterhalder@gmail.com>
2020-11-25 12:52:17 +0100

    Configured next release version
    - Set release to '0.4.21'
    - Set next snapshot to '0.4.22-SNAPSHOT'

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ git config --global credential.helper "store --file=.git/credentials"
 echo https://${GH_TOKEN}:@github.com > .git/credentials
 
 echo "Preparing maven release..."
-mvn --settings settings.xml -B release:prepare -Pdo-release -DreleaseVersion=0.4.20 -DdevelopmentVersion=0.4.21-SNAPSHOT
+mvn --settings settings.xml -B release:prepare -Pdo-release -DreleaseVersion=0.4.21 -DdevelopmentVersion=0.4.22-SNAPSHOT
 
 echo "Performing maven release..."
 mvn --settings settings.xml release:perform -Pdo-release
